7b50f18709021de84db1ce7915ec76d0829e9c33
Shane <shanepjennings@gmail.com>
2020-04-30 10:17:07 +0100

    Format json for encounters, game, and pokemon from API

--- pokemon_catch_flutter_app/lib/pokemon.dart
+++ pokemon_catch_flutter_app/lib/pokemon.dart
@@ -2,11 +2,22 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Game {
-  int _id;
   String name;
+
+  Game({this.name});
 
   static Future<List<Game>> loadGames() async {
     var url = "https://pokeapi.co/api/v2/version/?limit=40";
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      var gamesJson = json.decode(response.body);
+      Iterable l = gamesJson["results"];
+      return l.map((model) => Game.fromJson(json.decode(model));
+    }
+  }
+
+  factory Game.fromJson(Map<String, dynamic> resultJson) {
+    return Game(name: resultJson["name"]);
   }
 }
 
@@ -15,24 +26,30 @@ class Encounter {
   Game game;
   int chance;
 
-  static Future<List<Encounter>> getEncounters(int pokemonId) async {
+  Encounter({this.location, this.game, this.chance});
+
+  static Future<List<Encounter>> getEncountersForPokemon(int pokemonId) async {
     var url = "https://pokeapi.co/api/v2/pokemon/$pokemonId/encounters";
     final response = await http.get(url);
     if (response.statusCode == 200) {
       Iterable l = json.decode(response.body);
-      var encounters = l.map((model) => Encounter.fromJson(json.decode(model))).toList();
+      return l.map((model) => Encounter
+          .getEncountersForLocation(json.decode(model))).toList()
+          .expand((a) => a).toList();
     }
   }
 
-  factory Encounter.fromJson(Map<String, dynamic> json) {
-    String location = json['location_area']['name'];
-    Iterable l = json['version_details'];
-    for(var versionDetails in l) {
-      int maxChance = versionDetails.max_chance;
-      String game = versionDetails.version.name;
-    }
+  static List<Encounter> getEncountersForLocation(Map<String, dynamic> encounterForLocationJson) {
+    String location = encounterForLocationJson['location_area']['name'];
+    Iterable versionDetails = encounterForLocationJson['version_details'];
+    return versionDetails.map((version) => Encounter.fromJson(location, json.decode(version)));
 
-    return Encounter(json['location_area']['name'], json['version_details'][''], json['sprites']['front_default']);
+  }
+
+  factory Encounter.fromJson(String location, Map<String, dynamic> encounterDetailsForVersionJson) {
+    Game game = Game.getGame(encounterDetailsForVersionJson['version']['name']);
+    int maxChance = encounterDetailsForVersionJson['max_chance'];
+    return Encounter(location: location, game: game, chance: maxChance);
   }
 }
 
@@ -61,7 +78,7 @@ class Pokemon {
         pokemen.add(pokemon);
 
         // get encounters
-        var encounters = await Encounter.getEncounters(pokemon._id);
+        var encounters = await Encounter.getEncountersForPokemon(pokemon._id);
 
         // get games from encounters
         encounters.forEach((element) => pokemon._games.add(element.game));
